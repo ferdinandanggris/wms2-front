35e6962332d81b924cef4fa9ded8af13df8de8a1
FlomanB24 <gabriel.davinsi@binus.ac.id>
2023-07-11 14:48:11 +0700

    Mater Customer

---
+++ src/views/master/customer/CustomerList.js
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { FaLayerGroup } from "react-icons/fa";
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import ListWrapper from "../../../components/Wrapper/ListWrapper";
+import { refreshData, deleteData, exportData } from "../../../actions/data";
+
+const CustomerList = ({ user, data, refreshData, deleteData, exportData }) => {
+    const title = "Customer";
+    const img = <FaLayerGroup className="module-img" />;
+    const path = "/master/customer";
+    const url = "customer";
+    const role = "Master - Customer";
+
+    const columns = [
+        { label: "CODE", key: "voucherNo", width: 200 },
+        { label: "NAME", key: "projectID", width: 200 },
+        { label: "STATUS", key: "customerID", width: 200 },
+        { label: "ACTION#", key: "status", width: 200 },
+    ];
+
+    const exportFilename = "item-type.csv";
+
+    useEffect(() => {
+        if (user !== null) {
+            refreshData({ url });
+        }
+    }, [user, refreshData]);
+
+    return (
+        <ListWrapper
+            img={img}
+            title={title} path={path} url={url} exportFilename={exportFilename} role={role} columns={columns} data={data} refreshData={refreshData} exportData={exportData} deleteData={deleteData}
+        />
+    )
+};
+
+CustomerList.propTypes = {
+    user: PropTypes.object,
+    data: PropTypes.object,
+    refreshData: PropTypes.func,
+    deleteData: PropTypes.func,
+    exportData: PropTypes.func,
+};
+
+const mapStateToProps = (state) => ({
+    user: state.auth.user,
+    data: state.data,
+});
+
+export default connect(mapStateToProps, { refreshData, deleteData, exportData })(CustomerList);
