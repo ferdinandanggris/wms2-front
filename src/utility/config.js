da9b0748f479c12284aff9fe0c3d43a2eabc4af8
Sam <gbm1404@yahoo.com>
2023-07-11 11:06:29 +0700

    commit

--- src/utility/config.js
+++ src/utility/config.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:5000";
+const baseURL = "http://ws.mixtra.co.id";
 // const baseURL = process.env.REACT_APP_API_URL;
 const googleAPI = process.env.REACT_APP_GOOGLE_API;
 
